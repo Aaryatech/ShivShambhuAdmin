29f9e1b7ed4df60ff2b731afe98f414f3693da90
ats-12 <ats-12@ats12>
2020-11-19 17:35:36 +0530

    const-sac

--- src/main/java/com/ats/ssgs/common/Constants.java
+++ src/main/java/com/ats/ssgs/common/Constants.java
@@ -3,44 +3,44 @@ package com.ats.ssgs.common;
 public class Constants {
 
 	// Global
-	public static final String url = "http://localhost:8094/";
-	public static String REPORT_SAVE = "/home/lenovo/Documents/pdf/Report.pdf";
+	//public static final String url = "http://localhost:8087/";
+	//public static String REPORT_SAVE = "/home/lenovo/Documents/pdf/Report.pdf";
 
-	//public static final String url = "http://192.169.205.160:8080/ShivShambhuWebApi/";
-	//public static final String REPORT_SAVE = "apache-tomcat-8.5.40/webapps/uploads/shiv/Report.pdf";
+	public static final String url = "http://192.169.205.160:8080/ShivShambhuWebApi/";
+	public static final String REPORT_SAVE = "/home/shivstone/apache-tomcat-8.5.40/webapps/uploads/shiv/Report.pdf";
 
-	//public static final String ReportURL = "http://192.169.205.160:8080/ShivShambhuAdmin/";
-	public static final String ReportURL = "http://localhost:8081/ssgs/"; 
+	public static final String ReportURL = "http://192.169.205.160:8080/ShivShambhuAdmin/";
+	//public static final String ReportURL = "http://localhost:8081/ssgs/";
 	public static final String VEH_IMG_URL = "http://192.169.205.160:8080/uploads/shiv/Veh/";
 
 	public static final String WEIGHT_READING_URL = "http://192.169.205.160:8080/uploads/shiv/Weight/";
-	public static final String VEH_FOLDER = "apache-tomcat-8.5.40/webapps/uploads/shiv/Veh/";
-	public static final String WEIGHT_FOLDER = "apache-tomcat-8.5.40/webapps/uploads/shiv/Weight/";
+	public static final String VEH_FOLDER = "/home/shivstone/apache-tomcat-8.5.40/webapps/uploads/shiv/Veh/";
+	public static final String WEIGHT_FOLDER = "/home/shivstone/apache-tomcat-8.5.40/webapps/uploads/shiv/Weight/";
 
-	public static final String BILL_PDF = "apache-tomcat-8.5.40/webapps/uploads/shiv/bill.pdf";
-	public static final String QUOT_PDF = "apache-tomcat-8.5.40/webapps/uploads/shiv/quotation.pdf";
+	public static final String BILL_PDF = "/home/shivstone/apache-tomcat-8.5.40/webapps/uploads/shiv/bill.pdf";
+	public static final String QUOT_PDF = "/home/shivstone/apache-tomcat-8.5.40/webapps/uploads/shiv/quotation.pdf";
 
 	public static final int VEH_IMG_TYPE = 2;// fix for local and gloabal
 	public static final int WEIGHT_IMG_TYPE = 1;// fix for local and gloabal
 
 	/*
-	 * // Local public static final String ReportURL =
-	 * "http://localhost:8017/ssgs/"; public static final String url =
-	 * "http://localhost:8094/"; public static final String REPORT_SAVE =
-	 * "/home/lenovo/Desktop/Report.pdf";
-	 * 
-	 * 
-	 * // public static final String ReportURL = "http://localhost:8012/ssgs/";
-	 * public static final String VEH_IMG_URL =
-	 * "http://132.148.151.41:8080/shiv/Veh/"; public static final String
-	 * WEIGHT_READING_URL = "http://132.148.151.41:8080/shiv/Weight/"; public static
-	 * final String VEH_FOLDER = "/home/lenovo/Desktop/Vehicle"; public static final
-	 * String WEIGHT_FOLDER = "/home/lenovo/Desktop/Weight"; public static final
-	 * String BILL_PDF = "/home/lenovo/Desktop/bill.pdf"; public static final String
-	 * QUOT_PDF = "/home/lenovo/Desktop/quotation.pdf";
-	 */
+	* // Local public static final String ReportURL =
+	* "http://localhost:8017/ssgs/"; public static final String url =
+	* "http://localhost:8094/"; public static final String REPORT_SAVE =
+	* "/home/lenovo/Desktop/Report.pdf";
+	*
+	*
+	* // public static final String ReportURL = "http://localhost:8012/ssgs/";
+	* public static final String VEH_IMG_URL =
+	* "http://132.148.151.41:8080/shiv/Veh/"; public static final String
+	* WEIGHT_READING_URL = "http://132.148.151.41:8080/shiv/Weight/"; public static
+	* final String VEH_FOLDER = "/home/lenovo/Desktop/Vehicle"; public static final
+	* String WEIGHT_FOLDER = "/home/lenovo/Desktop/Weight"; public static final
+	* String BILL_PDF = "/home/lenovo/Desktop/bill.pdf"; public static final String
+	* QUOT_PDF = "/home/lenovo/Desktop/quotation.pdf";
+	*/
 
-//End
+	//End
 
 	public static final int CAT_FILE_TYPE = 0;
 
@@ -49,4 +49,5 @@ public class Constants {
 	// public static final Object VEH_IMG_URL = null;
 	public static int mainAct;
 	public static int subAct;
+
 }
